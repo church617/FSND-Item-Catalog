14bedb48e82e381422f602e8b15a7bb82ebef18d
Phillip Patterson <31708547+church617@users.noreply.github.com>
2018-09-09 11:01:36 -0500

    Replace missing parenthesis

--- application.py
+++ application.py
@@ -366,4 +366,4 @@ def logout():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='127.0.0.1', port=80
+    app.run(host='127.0.0.1', port=80)
